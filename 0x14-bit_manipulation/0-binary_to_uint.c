502d50b443c664a05badb52a9287bd9cfe39c546
hildasatih <hildasatih@gmail.com>
2023-09-01 18:21:54 -0700

    0-binary_to_uint.c  - Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int x;
-	unsigned int de_num  = 0;
+	unsigned int de_num = 0;
 
 	if (!b)
-		return (0)	
+		return (0);
+
 	for (x = 0; b[x]; x++)
 	{
 		if (b[x] < '0' || b[x] > '1')
 			return (0);
-		de_num  = 2 * +(b[x] - '0');
+		de_num = 2 * de_num + (b[x] - '0');
 	}
+
 	return (de_num);
 }
-
