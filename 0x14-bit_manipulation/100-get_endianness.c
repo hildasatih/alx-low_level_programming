a8ccbb82f7948bc77f94f03681a669111c747ab9
hildasatih <hildasatih@gmail.com>
2023-09-01 18:55:02 -0700

    100-get_endianness.c - Task 6. Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a machine is big or little endianness
+ * Return: returns for big 0, for little 1 
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *a = (char *) ax;
+
+	return (*a);
+}
